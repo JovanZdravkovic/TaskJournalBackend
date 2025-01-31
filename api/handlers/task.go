9cd132e7464c1c1ab78a0d49707479a7948a439b
Zdravkson <jovan.zdravkovic882@gmail.com>
2025-01-31 21:04:48 +0100

    Work done on tasks endpoint

--- api/handlers/task.go
+++ api/handlers/task.go
@@ -3,8 +3,15 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"regexp"
 
 	"github.com/JovanZdravkovic/TaskJournalBackend/db"
+)
+
+var (
+	TaskID       = regexp.MustCompile(`^/task/([a-fA-F0-9\-]{36})$`)
+	Tasks        = regexp.MustCompile(`^/tasks/*$`)
+	TasksStarred = regexp.MustCompile(`^/tasks/starred/*$`)
 )
 
 type TaskHandler struct {
@@ -12,6 +19,31 @@ type TaskHandler struct {
 }
 
 func (t *TaskHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch {
+	case r.Method == http.MethodGet && TaskID.MatchString(r.URL.Path):
+		t.GetTask(w, r)
+		return
+	case r.Method == http.MethodPost && TaskID.MatchString(r.URL.Path):
+		t.CreateTask(w, r)
+		return
+	case r.Method == http.MethodPut && TaskID.MatchString(r.URL.Path):
+		t.UpdateTask(w, r)
+		return
+	case r.Method == http.MethodDelete && TaskID.MatchString(r.URL.Path):
+		t.DeleteTask(w, r)
+		return
+	case r.Method == http.MethodGet && Tasks.MatchString(r.URL.Path):
+		t.GetTasks(w, r)
+		return
+	case r.Method == http.MethodGet && TasksStarred.MatchString(r.URL.Path):
+		t.GetStarredTasks(w, r)
+		return
+	default:
+		return
+	}
+}
+
+func (t *TaskHandler) GetTasks(w http.ResponseWriter, r *http.Request) {
 	tasks, err := t.DBService.GetTasks()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -24,6 +56,45 @@ func (t *TaskHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(err.Error()))
 		return
 	}
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(tasksJson)
+}
+
+func (t *TaskHandler) GetStarredTasks(w http.ResponseWriter, r *http.Request) {
+	tasks, err := t.DBService.GetStarredTasks()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	tasksJson, err := json.Marshal(tasks)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(tasksJson)
+}
+
+func (t *TaskHandler) GetTask(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("This is get single task endpoint"))
+}
+
+func (t *TaskHandler) CreateTask(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("This is create single task endpoint"))
+}
+
+func (t *TaskHandler) UpdateTask(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("This is update single task endpoint"))
+}
+
+func (t *TaskHandler) DeleteTask(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("This is delete single task endpoint"))
 }
--- db/db.go
+++ db/db.go
@@ -18,7 +18,7 @@ func NewDatabaseService(dbPool *pgxpool.Pool) *DatabaseService {
 }
 
 func (dbService *DatabaseService) GetTasks() ([]TaskDB, error) {
-	rows, err := dbService.pool.Query(context.Background(), "SELECT t.* FROM task t")
+	rows, err := dbService.pool.Query(context.Background(), "SELECT t.* FROM task t WHERE t.starred = false")
 	if err != nil {
 		return nil, errors.New("error while getting tasks from database")
 	} else {
@@ -44,3 +44,40 @@ func (dbService *DatabaseService) GetTasks() ([]TaskDB, error) {
 		return tasks, nil
 	}
 }
+
+func (dbService *DatabaseService) GetStarredTasks() ([]TaskDB, error) {
+	rows, err := dbService.pool.Query(context.Background(), "SELECT t.* FROM task t WHERE t.starred = true")
+	if err != nil {
+		return nil, errors.New("error while getting tasks from database")
+	} else {
+		var tasks []TaskDB
+		for rows.Next() {
+			var task TaskDB
+			err := rows.Scan(
+				&task.Id,
+				&task.TaskName,
+				&task.TaskIcon,
+				&task.TaskDesc,
+				&task.Deadline,
+				&task.Starred,
+				&task.Exec_status,
+				&task.Created_at,
+				&task.Created_by,
+			)
+			if err != nil {
+				return nil, errors.New("error while iterating dataset")
+			}
+			tasks = append(tasks, task)
+		}
+		return tasks, nil
+	}
+}
+
+// func (dbService *DatabaseService) GetTask() (TaskDB, error) {}
+
+// func (dbService *DatabaseService) CreateTask() (uuid.UUID, error) {}
+
+// // TODO: Updating a single task will have multiple functions for different kinds of updates
+// func (dbService *DatabaseService) PutTask() (string, error) {}
+
+// func (dbService *DatabaseService) DeleteTask() (string, error) {}
