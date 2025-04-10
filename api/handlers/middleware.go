db13372dde9020bd45800b8597156b951413b569
JovanZdravkovic <jovan.zdravkovic882@gmail.com>
2025-04-10 11:15:21 +0200

    Extracted CORS code into middleware

--- api/handlers/middleware.go
+++ api/handlers/middleware.go
@@ -8,6 +8,19 @@ import (
 	"github.com/google/uuid"
 )
 
+func EnableCORS(w http.ResponseWriter, r *http.Request) bool {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusNoContent)
+		return true
+	}
+	return false
+}
+
 func GetToken(r *http.Request) (*uuid.UUID, error) {
 	tokenCookie, err := r.Cookie("sessiontoken")
 	if err != nil {
@@ -47,6 +60,17 @@ func AuthMiddleware(next http.Handler, dbService db.DatabaseService) http.Handle
 		}
 
 		r.Header.Set("X-Auth-Token", token.String())
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		preflight := EnableCORS(w, r)
+		if preflight {
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
